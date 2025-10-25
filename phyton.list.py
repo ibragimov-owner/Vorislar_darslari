bd527aa2938a8a5e0138ddb5283d41b504136ae2
Ibragimov Muhammadali <ibragimovgamer@gmail.com>
2025-10-25 16:34:48 +0500

    max, min, sum,append,insert

--- phyton.list.py
+++ phyton.list.py
@@ -8,35 +8,57 @@
 """
 
 
+# davlatlar = ["Ozbekiston", "AQSH", "Rossiya", "Xitoy", "Turkiya"]
+# davlatlar.sort()
+# print(davlatlar)
+
+# mashinalar = list(("bmw", "audi", "Ferrari", "mersedes"))
+# mashinalar.sort()
+# print(mashinalar)
+
+# sonlar = list((0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+# sonlar.sort()
+# print(sonlar)
+
+# son = list((2, 9, 7, 5, 3, 1, 4, 6, 8, 0))
+# son.sort(reverse=True)
+# print(son)
+
+
+# sonlar1 = list(range(0, 21))
+# print(sonlar1)
+
+# sonlar2 = list(range(1, 30, 2))
+# print(sonlar2)
+
+# sonlar3 = list(range(-8, 30, 3))
+# print(sonlar3)
+# print(len(sonlar3))
+
+# sonlar4 = max((30, 0, -3, 15, 7, 22, 19))
+# print(sonlar4)
+
+# sonlar5 = min(30, 0, -3, 15, 7, 22, 19)
+# print(sonlar5)
+
+# sonlar6 = sum((30, 0, 3, 15, 7, 22, 19))
+# print(sonlar6)
+
+from calendar import c
+
+
 davlatlar = ["Ozbekiston", "AQSH", "Rossiya", "Xitoy", "Turkiya"]
-davlatlar.sort()
+
+country = davlatlar
+
+print("davlatlar nomli ro'yxat", davlatlar)
+print("country nomli ro'yxat", country)
+
+country.insert(0, "Angliya")
+print(country)
+
 print(davlatlar)
-
-mashinalar = list(("bmw", "audi", "Ferrari", "mersedes"))
-mashinalar.sort()
-print(mashinalar)
-
-sonlar = list((0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
-sonlar.sort()
-print(sonlar)
-
-son = list((2, 9, 7, 5, 3, 1, 4, 6, 8, 0))
-son.sort(reverse=True)
-print(son)
-
-
-sonlar1 = list(range(0, 21))
-print(sonlar1)
-
-sonlar2 = list(range(1, 30, 2))
-print(sonlar2)
-
-sonlar3 = list(range(-8, 30, 3))
-print(sonlar3)
-print(len(sonlar3))
-
-
-
+print(country)
 
 
 
