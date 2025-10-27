701a3b2f8f3ac64d068bd993fa94a69ccd0f4fcf
Ibragimov Muhammadali <ibragimovgamer@gmail.com>
2025-10-27 17:37:21 +0500

    mini game

---
+++ mini_game_1.100.py
@@ -0,0 +1,16 @@
+import random
+
+print("Men 1 dan 100 gacha bo'lgan son o'yladim. Topishga harakat qil!")
+son = random.randint(1, 100)
+taxminlar = 0
+
+while True:
+    taxmin = int(input("Taxminingni kiriting: "))
+    taxminlar += 1
+    if taxmin < son:
+        print("Kichik son aytding, yana urinib ko‘r!")
+    elif taxmin > son:
+        print("Katta son aytding, yana urin!")
+    else:
+        print(f"To‘g‘ri! 🎉 {taxminlar} urinishda topding!")
+        break
