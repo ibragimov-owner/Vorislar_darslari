ca90c2cd991f9798a43279ee38107f103aedcd2d
Ibragimov Muhammadali <ibragimovgamer@gmail.com>
2025-10-27 17:34:26 +0500

    sodda chat bot

---
+++ sodda_chatbot.py
@@ -0,0 +1,14 @@
+
+while True:
+    savol = input("Siz: ").lower()
+    if "salom" in savol:
+        print("Bot: Salom! Yaxshimisan?")
+    elif "yaxshi" in savol:
+        print("Bot: Zo‘r! Men ham yaxshi man 😎")
+    elif "isming" in savol:
+        print("Bot: Meni PyBot deb atashadi!")
+    elif "chiq" in savol or "xayr" in savol:
+        print("Bot: Xayr! 😊")
+        break
+    else:
+        print("Bot: Buni tushunmadim...")
