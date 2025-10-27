57d85bc3e953648e8909190d5d2589a86f25fa82
Ibragimov Muhammadali <ibragimovgamer@gmail.com>
2025-10-27 17:30:58 +0500

    real time yangilandi

--- real_time.py
+++ real_time.py
@@ -4,3 +4,13 @@ while True:
     vaqt = time.strftime("%H:%M:%S")
     print("⏰ Hozirgi vaqt:", vaqt, end="\r")
     time.sleep(1)
+
+# Ushbu kod har soniyada joriy vaqtni yangilab ko'rsatadi.
+
+# Konsolda vaqtni ko'rsatishda avvalgi vaqt ustiga yoziladi.
+
+# Dasturdan chiqish uchun klaviaturada Ctrl+C tugmalarini bosing.
+
+
+
+
