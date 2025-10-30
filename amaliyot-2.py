c448e9bb0606891820cb61c7d439fa431e6ff287
Ibragimov Muhammadali <ibragimovgamer@gmail.com>
2025-10-30 16:05:09 +0500

    amaliyot 2 appendded

--- amaliyot-2.py
+++ amaliyot-2.py
@@ -14,3 +14,32 @@
 · Ro'yxatning boshidan, o'rtasidan va oxiridan 20 ta qiymatni konsolga chiqaring
 · taomlar degan ro'yxat yarating va ichiga istalgan 5ta taomni kiriting
 """
+
+davlatlar = ["Ozbekiston", "AQSH", "Rossiya", "Xitoy", "Turkiya"]
+print(davlatlar)
+print("Ro'yxat uzunligi:", len(davlatlar))
+print("Tartiblangan ro'yxat:", sorted(davlatlar))
+print("Teskari tartiblangan ro'yxat:", sorted(davlatlar, reverse=True))
+print("Asl ro'yxat:", davlatlar)
+davlatlar.reverse()
+print("Ortidan boshlab ro'yxat:", davlatlar)
+print("Alifbo bo'yicha tartiblangan ro'yxat:", sorted(davlatlar))
+print("Alifboga teskari tartibda ro'yxat:", sorted(davlatlar, reverse=True))
+juft_sonlar = list(range(120, 1201, 2))
+print("Juft sonlar ro'yxati:", juft_sonlar)
+yigindi = sum(juft_sonlar)
+print("Sonlar yig'indisi:", yigindi)      
+eng_katta = max(juft_sonlar)
+eng_kichik = min(juft_sonlar)
+ayirma = eng_katta - eng_kichik
+print("Eng katta va eng kichik son o'rtasidagi ayirma:", ayirma)
+print("Elementlar soni:", len(juft_sonlar))
+print("Ro'yxatning boshidan 20 ta qiymat:", juft_sonlar[:20])
+print("Ro'yxatning o'rtasidan 20 ta qiymat:", juft_sonlar[len(juft_sonlar)//2-10:len(juft_sonlar)//2+10])
+print("Ro'yxatning oxiridan 20 ta qiymat:", juft_sonlar[-20:])
+taomlar = ["osh", "shashlik", "manti", "lag'mon", "somsa"]
+print("Taomlar ro'yxati:", taomlar)
+
+
+
+
