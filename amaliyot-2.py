f76751cbcbbe972b68cde9880ae26c165152a170
Ibragimov Muhammadali <ibragimovgamer@gmail.com>
2025-10-30 16:04:43 +0500

    amaliyot 2

---
+++ amaliyot-2.py
@@ -0,0 +1,16 @@
+"""
+··Shartlar:
+· O'zingizga ma'lum davlatlarning ro'yxatini tuzing va ro'yxatni konsolga chiqaring
+· Ro'yxatning uzunligini konsolga chiqaring
+· sorted () funktsiyasi yordamida ro'yxatni tartiblangan holda konsolga chiqaring
+· sorted () yordamida ro'yxatni teskari tartibda konsolga chiqaring
+· Asl ro'yxatni qaytadan konsolga chiqaring
+· reverse () metodi yordamida ro'yxatni ortidan boshlab chiqaring
+· sort () metodi yordamida ro'yxatni avval alifbo bo'yicha, keyin esa alifboga teskari tartibda konsolga chiqaring.
+· 120 dan 1200 gacha bo'lgan juft sonlar ro'yxatini tuzing
+· Ro'yxatdagi sonlar yig'indisini hisoblang va konsolga chiqaring
+· Ro'yxatdagi eng katta va eng kichik son o'rtasidagi ayirmani hisoblang va konsolga chiqaring
+· Ro'yxatdagi elementlar sonini hisoblang
+· Ro'yxatning boshidan, o'rtasidan va oxiridan 20 ta qiymatni konsolga chiqaring
+· taomlar degan ro'yxat yarating va ichiga istalgan 5ta taomni kiriting
+"""
