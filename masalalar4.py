2b545073a404549f989e358f1cc777c99f909192
Ibragimov Muhammadali <ibragimovgamer@gmail.com>
2025-11-04 16:23:49 +0500

    masalalar 4 , 4 gacga

--- masalalar4.py
+++ masalalar4.py
@@ -1,5 +1,6 @@
          # masala 1
 # for1: k va n butun sonlar berilgan (n>0) . k sonini n marta chiqaruvchi programma tuzilsin
+# print("k sonini n marta chiqaruvchi programma")
 # k = int(input("k sonini kiriting:"))
 # n = int(input("n sonini kiriting: (n>0):"))
 
@@ -7,29 +8,44 @@
 #     print(k)
 
 
+
          # masala 2
-# For3. a va b butun sonlari berilgan (a <b). a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma tuzilsin.
+# For2. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlami (a va b ni ham) chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin. (a va b xam chiqarilsin).
+# a = int(input("a ni kiriting (a < b): "))
+# b = int(input("b ni kiriting: "))
+
+
+# son = 0
+
+# print("a dan b gacha bo'lgan sonlar:")
+
+# for i in range(a, b + 1):
+#     print(i)
+#     son += 1
+
+# print("Chiqarilgan sonlar soni:", son)
 
 
 
-
-
-
-
-
-
-
-
+         # masala 3
+# For3. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma tuzilsin.
+# a = int(input("a sonini kiriting (a<b): "))
+# b = int(input("b sonini kiriting (a<b): "))
+# son = 0
+# for i in range(b-1, a, -1):
+#       print(i)
+#       son += 1
+# print(f"Chiqarilgan sonlar soni: {son}")
 
 
 
       # masala 4
 # for4: Bir kg konfetning narxi berilgan (haqiqiy son). 1, 2, ... , 10 kg konfetni narxini chiqaruvchi programma tuzilsin.
-# narx = float(input("Bir kg shakarning narxini kiriting: "))
+# narx = float(input("Bir kg konfetning narxini kiriting: "))
 
 # for kg in range(1, 11):
 #     summa = kg * narx
-#     print(f"{kg} kg shakarning narxi: {summa} so'm")
+#     print(f"{kg} kg konfetning narxi: {summa} so'm")
 
 
 
