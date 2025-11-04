662917bae0a5a3246e973fe9e9d388954a22474d
Ibragimov Muhammadali <ibragimovgamer@gmail.com>
2025-11-04 16:03:04 +0500

    append

--- masalalar4.py
+++ masalalar4.py
@@ -1,15 +1,35 @@
+         # masala 1
 # for1: k va n butun sonlar berilgan (n>0) . k sonini n marta chiqaruvchi programma tuzilsin
-k = int(input("k sonini kiriting:"))
-n = int(input("n sonini kiriting: (n>0):"))
+# k = int(input("k sonini kiriting:"))
+# n = int(input("n sonini kiriting: (n>0):"))
 
-for i in range(n):
-    print(k)
+# for i in range(n):
+#     print(k)
 
 
+         # masala 2
+# For3. a va b butun sonlari berilgan (a <b). a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma tuzilsin.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      # masala 4
 # for4: Bir kg konfetning narxi berilgan (haqiqiy son). 1, 2, ... , 10 kg konfetni narxini chiqaruvchi programma tuzilsin.
-narx = (input("Bir kg shakarning narxini kiriting: "))
-for sonlar in haqiqiy_son:
-    print(f"{sonlar} kg shakar")  
+# narx = float(input("Bir kg shakarning narxini kiriting: "))
+
+# for kg in range(1, 11):
+#     summa = kg * narx
+#     print(f"{kg} kg shakarning narxi: {summa} so'm")
 
 
 
